8bddccdbad1a81877f4a1950e1995c58101e26f2
lixueyue <lixueyue@xinyu668.com>
2020-10-12 20:51:57 +0800

    添加了 IsExistSortedNums

--- array/array.go
+++ array/array.go
@@ -8,3 +8,19 @@ func IsExist(nums []int, targetNum int) bool {
 	}
 	return false
 }
+
+func IsExistSortedNums(nums []int,targetNum int) bool{
+	left,right := 0,len(nums)-1
+	for left<=right{
+		mid := left+(right-left)/2
+		if nums[mid]==targetNum{
+			return true
+		}
+		if nums[mid]>targetNum{
+			right = mid - 1
+		}else{
+			left = mid + 1
+		}
+	}
+	return false
+}
