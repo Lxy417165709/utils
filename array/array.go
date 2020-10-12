284081562d3e007042c92d7d13681e7f90aff7eb
Lxy417165709 <417165709@qq.com>
2020-10-12 23:16:11 +0800

    添加了一些函数

--- array/array.go
+++ array/array.go
@@ -1,6 +1,26 @@
 package array
 
-func IsExist(nums []int, targetNum int) bool {
+type Int int
+
+type Comparable interface {
+	Greater(c Comparable) bool
+}
+
+func (i Int) Greater(c Comparable) bool {
+	j := c.(Int)
+	return i > j
+}
+
+
+func IntsToInterfaces(ints []int) []interface{} {
+	interaces := make([]interface{},0)
+	for _,i := range ints{
+		interaces = append(interaces,i)
+	}
+	return interaces
+}
+
+func IsExist(nums []interface{}, targetNum interface{}) bool {
 	for _, num := range nums {
 		if num == targetNum {
 			return true
@@ -9,14 +29,14 @@ func IsExist(nums []int, targetNum int) bool {
 	return false
 }
 
-func IsExistInSortedNums(nums []int, targetNum int) bool {
+func IsExistInSortedArray(nums []Comparable, targetNum Comparable) bool {
 	left, right := 0, len(nums)-1
 	for left <= right {
 		mid := left + (right-left)/2
 		if nums[mid] == targetNum {
 			return true
 		}
-		if nums[mid] > targetNum {
+		if nums[mid].Greater(targetNum) {
 			right = mid - 1
 		} else {
 			left = mid + 1
