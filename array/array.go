fb2eb6de6a5f993a59c2fa44cb2adf7e9dd14726
lixueyue <lixueyue@xinyu668.com>
2020-10-30 10:28:49 +0800

    添加快速排序框架

--- array/array.go
+++ array/array.go
@@ -1,19 +1,19 @@
 package array
 
-
 type Comparable interface {
 	Greater(c Comparable) bool
 }
 
 func IntsToInterfaces(ints []int) []interface{} {
-	interaces := make([]interface{},0)
-	for _,i := range ints{
-		interaces = append(interaces,i)
+	interaces := make([]interface{}, 0)
+	for _, i := range ints {
+		interaces = append(interaces, i)
 	}
 	return interaces
 }
 
-func IsExist(nums []interface{}, targetNum interface{}) bool {
+// ---------------- 查找相关 ----------------
+func IsExist(nums []Comparable, targetNum Comparable) bool {
 	for _, num := range nums {
 		if num == targetNum {
 			return true
@@ -55,11 +55,48 @@ func GetFirstGreaterOrEqualIndex(nums []Comparable, targetNum Comparable) int {
 	left, right := 0, len(nums)-1
 	for left <= right {
 		mid := left + (right-left)/2
-		if targetNum.Greater(nums[mid]) {
+		if isGreaterOrEqual(nums[mid], targetNum) {
 			left = mid + 1
 		} else {
 			right = mid - 1
 		}
 	}
 	return left
+}
+
+// ---------------- 排序相关 ----------------
+func QuickSort(nums []Comparable) {
+	// 要加这个限制，否则 right 可能等于 -1
+	if len(nums) == 0 {
+		return
+	}
+
+	refNumIndex := 0
+	left, right := 0, len(nums)-1
+	for left <= right {
+		for left <= right && isSmallerOrEqual(nums[left], nums[refNumIndex]) {
+			left++
+		}
+		for left <= right && isGreaterOrEqual(nums[right], nums[refNumIndex]) {
+			right--
+		}
+		if left <= right {
+			nums[right], nums[left] = nums[left], nums[right]
+		}
+	}
+	nums[right], nums[refNumIndex] = nums[refNumIndex], nums[right]
+	QuickSort(nums[:right])
+	QuickSort(nums[right+1:])
+}
+
+func isEqual(firstNum Comparable, secondNum Comparable) bool {
+	return !firstNum.Greater(secondNum) && !secondNum.Greater(firstNum)
+}
+
+func isGreaterOrEqual(firstNum Comparable, secondNum Comparable) bool {
+	return firstNum.Greater(secondNum) || isEqual(firstNum, secondNum)
+}
+
+func isSmallerOrEqual(firstNum Comparable, secondNum Comparable) bool {
+	return !firstNum.Greater(secondNum)
 }
--- array/array_test.go
+++ array/array_test.go
@@ -30,3 +30,17 @@ func Test(t *testing.T) {
 		String("123"), String("456"), String("789"),
 	}, String("456")))
 }
+
+func TestQuickSort(t *testing.T) {
+	nums := []Comparable{
+		Int(1), Int(8), Int(2),
+		Int(4), Int(8), Int(7),
+		Int(6), Int(7), Int(89),
+		Int(1), Int(98), Int(3),
+		Int(18), Int(9), Int(3),
+		Int(18), Int(-5), Int(123),
+	}
+	fmt.Println("Before sorting", nums)
+	QuickSort(nums)
+	fmt.Println("After sorting", nums)
+}
