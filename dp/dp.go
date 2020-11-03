cf513aa5041342490eb9e4cf36362e916eee6afd
Lxy417165709 <417165709@qq.com>
2020-11-04 01:11:49 +0800

    .

--- dp/dp.go
+++ dp/dp.go
@@ -20,7 +20,7 @@ func LongestSubArray(arr1 []interface{}, arr2 []interface{}) []interface{} {
 	return result
 }
 
-// 未测试
+// ----------------------- 最长公共子序列 -----------------------
 func LongestSubSequence(arr1 []interface{}, arr2 []interface{}) []interface{} {
 	lss := make([][][]interface{}, len(arr1)+1)
 	for i := 0; i < len(lss); i++ {
@@ -30,18 +30,18 @@ func LongestSubSequence(arr1 []interface{}, arr2 []interface{}) []interface{} {
 	for i := 1; i <= len(arr1); i++ {
 		for t := 1; t <= len(arr2); t++ {
 			if arr1[i-1] == arr2[t-1] {
-				lss[i][t] = longestArray(
-					append(lss[i-1][t-1], arr1[i-1]),
-					lss[i-1][t],
-					lss[i][t-1],
-				)
+				//lss[i][t] = longestArray(
+				//	append(lss[i-1][t-1], arr1[i-1]),
+				//	lss[i-1][t],
+				//	lss[i][t-1],
+				//)	这种写法也可以
+				lss[i][t] = append(lss[i-1][t-1], arr1[i-1])
 			} else {
 				lss[i][t] = longestArray(
 					lss[i-1][t],
 					lss[i][t-1],
 				)
 			}
-
 			if len(lss[i][t]) > len(result) {
 				result = lss[i][t]
 			}
