1845a6c5cc9bf0073fe3d450c7b9b30419d88172
Lxy417165709 <417165709@qq.com>
2020-11-07 01:18:19 +0800

    .

--- dp/dp.go
+++ dp/dp.go
@@ -1,7 +1,47 @@
 package dp
 
+import "github.com/Lxy417165709/utils/array"
+
+// ----------------------- 最长递增子串 -----------------------
+func LongestIncrementSubArray(nums []array.Comparable) []array.Comparable {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	lis := make([][]array.Comparable, len(nums))
+	lis[0] = []array.Comparable{
+		nums[0],
+	}
+	result := lis[0]
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i].Greater(nums[i-1]) {
+			lis[i] = append(lis[i-1], nums[i])
+		} else {
+			lis[i] = []array.Comparable{nums[i]}
+		}
+		if len(lis[i]) > len(result) {
+			result = lis[i]
+		}
+	}
+	return result
+}
+
 // ----------------------- 最长公共子串 -----------------------
 func LongestSubArray(arr1 []interface{}, arr2 []interface{}) []interface{} {
+	/*
+		dp[i][t] -> 以 arr[:i] 为结尾, 及以 arr2[:t] 为结尾的最长公共子串
+
+		则有如下关系式
+
+		dp[i][t] = {
+			if i == 0: 0
+			if t == 0: 0
+			if arr1[i-1] == arr2[t-1]: dp[i-1][t-1] + 1
+			else: 0
+		}
+	*/
+
 	lsa := make([][][]interface{}, len(arr1)+1)
 	for i := 0; i < len(lsa); i++ {
 		lsa[i] = make([][]interface{}, len(arr2)+1)
@@ -22,6 +62,18 @@ func LongestSubArray(arr1 []interface{}, arr2 []interface{}) []interface{} {
 
 // ----------------------- 最长公共子序列 -----------------------
 func LongestSubSequence(arr1 []interface{}, arr2 []interface{}) []interface{} {
+	/*
+		dp[i][t] -> 以 arr[:i] 为结尾, 及以 arr2[:t] 为结尾的最长公共子序列
+
+		则有如下关系式
+
+		dp[i][t] = {
+			if i == 0: 0
+			if t == 0: 0
+			if arr1[i-1] == arr2[t-1]: dp[i-1][t-1] + 1
+			else: max(dp[i-1][t], dp[i][t-1])
+		}
+	*/
 	lss := make([][][]interface{}, len(arr1)+1)
 	for i := 0; i < len(lss); i++ {
 		lss[i] = make([][]interface{}, len(arr2)+1)
@@ -30,11 +82,6 @@ func LongestSubSequence(arr1 []interface{}, arr2 []interface{}) []interface{} {
 	for i := 1; i <= len(arr1); i++ {
 		for t := 1; t <= len(arr2); t++ {
 			if arr1[i-1] == arr2[t-1] {
-				//lss[i][t] = longestArray(
-				//	append(lss[i-1][t-1], arr1[i-1]),
-				//	lss[i-1][t],
-				//	lss[i][t-1],
-				//)	这种写法也可以
 				lss[i][t] = append(lss[i-1][t-1], arr1[i-1])
 			} else {
 				lss[i][t] = longestArray(
